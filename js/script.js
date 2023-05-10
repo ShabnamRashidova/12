e36992498d4216376a3c643f4204bdf92eb1d370
Shebnem <shabnamrashidova90@gmail.com>
2023-05-10 13:11:05 +0400

    add animation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-$(function(){
-    $(".list-item").click(function() {
-        $(this).toggleClass("active");
-        $(".list-item").not(this).removeClass("active");
-      });
-   $(".single-item-header").click(function(){
+$(function () {
+  $(".list-item").click(function () {
+    $(this).toggleClass("active");
+    $(".list-item").not(this).removeClass("active");
+  });
+  $(".single-item-header").click(function () {
+    $(".single-item-body").hide();
     $(this).next(".single-item-body").slideToggle();
-    $(this).find("i").toggleClass('active');
-   })
-   
-})
+
+    $(this).find("i").toggleClass("active");
+  });
+});
