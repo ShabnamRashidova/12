bf46ed1c0f9a5233dda4662d435ab3825423093e
Shebnem <shabnamrashidova90@gmail.com>
2023-05-10 14:00:24 +0400

    filiallar

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ $(function () {
     $(this).toggleClass("active");
     $(".list-item").not(this).removeClass("active");
   });
-  $(".single-item-header").click(function () {
-    $(".single-item-body").hide();
-    $(this).next(".single-item-body").slideToggle();
 
+  $(".single-item-header").click(function () {
+    var single_body = $(this).next(".single-item-body");
+    single_body.slideToggle(200);
+    $(".single-item-body").not(single_body).hide();
     $(this).find("i").toggleClass("active");
   });
 });
